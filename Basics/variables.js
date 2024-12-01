e071df93b5cf092e0016ce69873ee6763a3ef1bb
Priyanka-iic <46787799+Priyanka-iic@users.noreply.github.com>
2024-12-01 13:23:38 +0000

    commit second

---
+++ Basics/variables.js
@@ -0,0 +1,19 @@
+const account_id = 1440
+let account_email = "example.com"
+var account_pwd= "S123d"
+account_city = "Delhi"
+//not a good way to use
+
+let account_state;
+
+//account_id = 1220 //not allowed
+console.log(account_id);
+account_emil = "Qbec.com"
+account_pwd = "2SSER"
+account_city = "Haryana"
+
+console.table([account_id, account_email, account_pwd, account_city, account_state])
+
+/*
+Dont use var due to scope issues
+*/
