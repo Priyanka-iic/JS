4b5f542cde45e304b37029a49bf4652d98f6f708
Priyanka-iic <46787799+Priyanka-iic@users.noreply.github.com>
2024-12-01 13:49:26 +0000

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,32 @@
+/*
+"use strict"; > treat all JS code as newer version
+*/
+
+//alert("hello") //using nodejs not browser
+
+
+let name = "String data type"
+let age = 20
+let isboolean = true
+
+//number data type has some range
+//for some bigger number we can use bigint
+//string data type can be used either with single or double quotes
+//boolean we know use for comparison
+
+//null is a value >> stand alone value
+
+//undefined > whoes value is not defiend
+
+let state = null
+
+
+//symbol >> unique
+
+//object non premitive data type
+
+//with typeof we can know the data type and here null is defined as object type
+
+console.log(typeof state)
+
+
