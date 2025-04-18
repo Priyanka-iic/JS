660f57e902fd21c600d8f148c4dc6c122ca7710a
Priyanka-iic <46787799+Priyanka-iic@users.noreply.github.com>
2025-04-18 18:11:21 +0000

    strings

---
+++ Basics/String.js
@@ -0,0 +1,47 @@
+//the old way
+
+let name = "Priyanka";
+let age = 27;
+//console.log("My name is "+name+" and my age is "+age);
+
+//the new way: String interpolation
+
+/*console.log(`My name is ${name} and my age is ${age}`);
+
+console.log(name[0]);
+console.log(name.toUpperCase());
+console.log(name.concat("Arora"));
+console.log(name.at());
+console.log(name.length);
+
+console.log(name.bold());
+
+let val = "  tri  ";
+const val2 = val.trim();
+console.log(val.length);
+
+console.log(val);
+
+console.log(val2.length);
+
+console.log(val2); */
+
+
+// ALL FUNCTIONS & METHODS
+const test = new String('John Doe')
+const testTwo = "Jane Doe"
+
+console.log(testTwo);
+
+console.log(test);
+
+console.log(test.at());
+console.log(test.charAt(5));
+
+
+
+
+
+
+
+
