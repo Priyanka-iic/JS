12eb68614a19baf569c9c684f475eb1b6c5d7df6
Priyanka-iic <46787799+Priyanka-iic@users.noreply.github.com>
2025-04-18 14:25:46 +0000

    Comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,25 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//Comparison should never be done with different data types as results might be unperdictable"
+
+console.log("2">1);
+console.log("02">1);
+console.log(null>1);
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
+console.log(undefined>1);
+console.log(undefined>0);
+
+//strict check with ===
+console.log("Strict Check also checks the data type");
+
+console.log("2"===2);
+console.log("2"==="2");
+
+
